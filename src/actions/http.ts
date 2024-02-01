55f853962c204423eec1b8c2cb25336b7d3d4fd6
Agustin Kassis <webmaster@masize.com>
2024-02-01 20:03:53 -0300

    added http action mock

--- src/actions/http.ts
+++ src/actions/http.ts
@@ -1,1 +1,11 @@
-export
+import { NostrEvent } from 'nostr-tools';
+
+export default function (event: NostrEvent) {
+  console.info('Executed HTTP');
+  console.dir(event);
+
+  //   return fetch('https://api.nostr.com/events', {
+  //     method: 'POST',
+  //     body: JSON.stringify({ ...event }),
+  //   });
+}
