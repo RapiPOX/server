c9c7dd09aa60edb2ddeb1da3ce2e2c3e2c632b63
Agustin Kassis <webmaster@masize.com>
2024-02-24 01:43:06 -0300

    added lnurl tupe

--- src/types/lnurl.ts
+++ src/types/lnurl.ts
@@ -1,8 +1,17 @@
-export interface LNURLw {
+export interface LNURLResponse {
   tag: string;
   callback: string;
-  k1: string;
-  defaultDescription: string;
-  minWithdrawable: number;
-  maxWithdrawable: number;
+  k1?: string;
+  metadata?: string;
+  commentAllowed?: number;
+  minWithdrawable?: number;
+  maxWithdrawable?: number;
+  minSendable?: number;
+  maxSendable?: number;
+  payerData?: { [_key: string]: { mandatory: boolean } };
+  defaultDescription?: string;
+  nostrPubkey?: string;
+  allowsNostr?: boolean;
+  accountPubKey?: string;
+  federationId?: string;
 }
